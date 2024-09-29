6a8ecf9c8715f69820e634c7ca4662cb0cd36ef4
anybody1234 <167839131+anybody1234@users.noreply.github.com>
2024-09-29 12:46:18 +0700

    ChoosingOption.java

--- ChoosingOption.java
+++ ChoosingOption.java
@@ -3,6 +3,7 @@ public class ChoosingOption{
 	public static void main(String[] args){
 		int option = JOptionPane.showConfirmDialog(null,"Do you want to change the first class ticket?");
 		JOptionPane.showMessageDialog(null,"You've chosen: "+(option==JOptionPane.YES_OPTION?"Yes":"No"));
+		option = JOptionPane.showConfirmDialog(null, "Are you sure", "Select an Option", JOptionPane.YES_NO_OPTION);
 		System.exit(0);
 }
 }
